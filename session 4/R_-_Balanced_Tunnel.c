6423c79a8998d9254757fbd30e627d4a60145099
Mehedi Hasan Maruf <41261534+Mehedi-CSE20@users.noreply.github.com>
2022-04-16 05:29:01 +0600

    Update R_-_Balanced_Tunnel.c

--- session 4/R_-_Balanced_Tunnel.c
+++ session 4/R_-_Balanced_Tunnel.c
@@ -6,32 +6,21 @@ int out[100005];
 int main() {
     int i, c, f, x;
     scanf("%d", &n);
-    for (i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) {
             scanf("%d", &x);
             in[x] = i;
     }
-    for (i = 0; i <= n; i++) {
-        printf("in: %d ", in[i]);
-    }
-    c = 0;
-    f = 0;
-    for (i = 0; i < n; i++) {
-        scanf("%d", &x);
-        // printf("x: %d ", x);
-        x = in[x];
-        out[x] = 1;
-        // printf("%d\n", x);
-        // printf("%d %d", c, f);
-        // printf("Oin: %d\n", out[i-1]);
-        if (x > c) {
-                f++;
-        }
-        while (out[c]) {
-                c++;
-        }
-    }
-    for (i = 0; i <= n; i++) {
-        printf("o: %d\n", out[i]);
+    c = f = 0;
+    for(i = 0; i < n; i++) {
+            scanf("%d", &x);
+            x = in[x];
+            out[x] = 1;
+            if(x > c) {
+                    f++;
+            }
+            while(out[c]) {
+                    c++;
+            }
     }
     printf("%d\n", f);
     return 0;
